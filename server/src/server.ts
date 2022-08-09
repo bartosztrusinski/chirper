1e4c170b1926ecfe59ae70e55ee6fb495741d5f8
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-08-09 02:49:44 +0200

    setup dotenv

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
