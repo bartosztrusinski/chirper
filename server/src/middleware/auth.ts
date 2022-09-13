a1108a54f85eb12db12c6e519dd3bc7fa31a0e04
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-09-14 01:49:15 +0200

    apply validation types in middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,16 +1,21 @@
-import { Handler } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { Types } from 'mongoose';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from '../models/User';
 import { BadRequestError, UnauthorizedError } from '../utils/errors';
 import { JWT_SECRET } from '../config/secrets';
 import { Chirp } from '../models/Chirp';
+import { ChirpId } from '../schemas';
 
 interface AuthPayload extends JwtPayload {
   currentUserId: Types.ObjectId;
 }
 
-export const isAuthenticated: Handler = async (req, res, next) => {
+export const isAuthenticated = async (
+  req: Request<unknown, unknown, unknown, unknown>,
+  res: Response,
+  next: NextFunction
+) => {
   //get auth header ✅
   const authHeader = req.header('authorization');
 
@@ -46,7 +51,11 @@ export const isAuthenticated: Handler = async (req, res, next) => {
   next();
 };
 
-export const isChirpAuthor: Handler = async (req, res, next) => {
+export const isChirpAuthor = async (
+  req: Request<ChirpId, unknown, unknown, unknown>,
+  res: Response,
+  next: NextFunction
+) => {
   const { currentUserId } = req;
   const { chirpId } = req.params;
 
@@ -56,11 +65,10 @@ export const isChirpAuthor: Handler = async (req, res, next) => {
     );
   }
 
-  const foundChirp = await Chirp.findById(chirpId);
+  const foundChirp = await Chirp.findById(chirpId).select('author');
   if (!foundChirp) {
     throw new BadRequestError('Sorry, we could not find that chirp');
   }
-
   if (!foundChirp.author.equals(currentUserId)) {
     throw new UnauthorizedError(
       'Not authorized, you must be the author of the chirp to do that'
@@ -70,11 +78,15 @@ export const isChirpAuthor: Handler = async (req, res, next) => {
   next();
 };
 
-export const confirmPassword: Handler = async (req, res, next) => {
+export const confirmPassword = async (
+  req: Request<unknown, unknown, { password: string }, unknown>,
+  res: Response,
+  next: NextFunction
+) => {
   const { currentUserId } = req;
   const { password } = req.body;
 
-  const currentUser = await User.findById(currentUserId);
+  const currentUser = await User.findById(currentUserId).select('password');
   if (!currentUser) {
     throw new BadRequestError('Sorry, we could not find your account');
   }
--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -1,5 +1,4 @@
-import { Handler } from 'express';
-import { Types } from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 
 export interface RequestValidators {
@@ -10,8 +9,12 @@ export interface RequestValidators {
 }
 
 export const validateRequest =
-  (validators: RequestValidators): Handler =>
-  (req, res, next) => {
+  (validators: RequestValidators) =>
+  (
+    req: Request<unknown, unknown, unknown, unknown>,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
       if (validators.body) {
         req.body = validators.body.parse(req.body);
@@ -32,83 +35,3 @@ export const validateRequest =
       }
     }
   };
-
-const transformOptionalFields = (...allowedFields: string[]) =>
-  z
-    .function()
-    .args(z.string().optional())
-    .returns(z.string())
-    .implement((fields) => {
-      fields = fields ? fields + `,${allowedFields[0]}` : allowedFields[0];
-      return fields
-        .split(',')
-        .map((field) => field.trim().toLowerCase())
-        .filter((field) => {
-          return allowedFields
-            .map((field) => field.toLowerCase())
-            .includes(field);
-        })
-        .reduce((str, field) => {
-          if (field === 'createdat') {
-            field = 'createdAt';
-          }
-          return str + field + ' ';
-        }, '');
-    });
-
-export const transformUserFields = transformOptionalFields(
-  'username',
-  'profile',
-  'metrics',
-  '_id',
-  'createdAt'
-);
-
-export const transformChirpFields = transformOptionalFields(
-  'content',
-  'author',
-  'replies',
-  'post',
-  'parent',
-  'metrics',
-  '_id',
-  'createdat'
-);
-
-export const transformExpandAuthor = z
-  .function()
-  .args(z.string().optional())
-  .returns(z.boolean())
-  .implement((expandAuthor) => {
-    return expandAuthor === 'true';
-  });
-
-export const currentUserIdSchema = z.instanceof(Types.ObjectId, {
-  message: 'Current user id must be valid',
-});
-
-export type CurrentUserId = z.infer<typeof currentUserIdSchema>;
-
-export const usernameSchema = z.object({
-  username: z.string({
-    invalid_type_error: 'Username must be a string',
-  }),
-});
-
-export type Username = z.infer<typeof usernameSchema>;
-
-export const chirpIdSchema = z.object({
-  chirpId: z.string().transform((id, ctx) => {
-    try {
-      return new Types.ObjectId(id);
-    } catch (error) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: 'ChirpId must be valid',
-      });
-      return z.NEVER;
-    }
-  }),
-});
-
-export type ChirpId = z.infer<typeof chirpIdSchema>;
