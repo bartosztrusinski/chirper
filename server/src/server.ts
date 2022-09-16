b185f3555744d84f65a02622e9b5bf2fe89081af
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-09-16 16:10:16 +0200

    use api routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config';
 import express, { Application } from 'express';
 import 'express-async-errors';
-import userRoutes from './routes/user';
-import chirpRoutes from './routes/chirp';
-import currentUserRoutes from './routes/currentUser';
 import errorHandler from './middleware/error';
 import connectDB from './config/db';
 import { NotFoundError } from './utils/errors';
+import apiRoutes from './routes';
 
 connectDB();
 
@@ -16,9 +14,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users', userRoutes);
-app.use('/api/chirps', chirpRoutes);
-app.use('/api/me', currentUserRoutes);
+app.use('/api', apiRoutes);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((req, res, next) => {
