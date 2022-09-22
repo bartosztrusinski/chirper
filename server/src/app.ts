5729aa6ded14ca806ba11d591d951874b346e2ea
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-09-23 00:38:35 +0200

    separate app from server

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application } from 'express';
+import 'express-async-errors';
+import apiRoutes from './routes';
+import notFound from './middleware/notFound';
+import errorHandler from './middleware/error';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,13 @@
 import 'dotenv/config';
-import express, { Application } from 'express';
-import 'express-async-errors';
 import connectDB from './config/db';
-import apiRoutes from './routes';
-import errorHandler from './middleware/error';
-import notFound from './middleware/notFound';
+import app from './app';
 import config from './config/general';
 
 connectDB();
 
-const app: Application = express();
+const port = config.app.port;
+const env = config.app.environment;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', apiRoutes);
-
-app.use(notFound);
-app.use(errorHandler);
-
-app.listen(config.app.port, () => {
-  console.log(
-    `Server is running on port ${config.app.port} in ${config.app.environment} mode`
-  );
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} in ${env} mode`);
 });
