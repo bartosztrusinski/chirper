b3aa7e9f2a55806746fdd1f68c97055629672994
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-09-28 19:43:09 +0200

    init webpack

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+const daedricSwordRecipe = {
+  name: "Daedric Sword",
+  ingredients: [
+    { name: "Daedra Heart", quantity: 1 },
+    { name: "Leather Strips", quantity: 1 },
+    { name: "Ebony Ingot", quantity: 2 },
+  ],
+  skill: "Smithing",
+  skillLevel: 90,
+  type: "Daedric",
+};
+
+console.log("Daedric sword recipe: ", daedricSwordRecipe);
