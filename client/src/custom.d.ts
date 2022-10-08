8877c96ecb25c92aadbf236110b9473c169e5d35
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-08 23:59:26 +0200

    use css modules

--- client/src/custom.d.ts
+++ client/src/custom.d.ts
@@ -23,17 +23,17 @@ declare module '*.svg' {
   export default content;
 }
 
-declare module '*.css' {
-  const content: string;
-  export default content;
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
 }
 
-declare module '*.scss' {
-  const content: string;
-  export default content;
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
 }
 
-declare module '*.sass' {
-  const content: string;
-  export default content;
+declare module '*.module.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
 }
