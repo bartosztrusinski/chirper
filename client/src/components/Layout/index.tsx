09a848530ee139948088b694bc0fc9d2b89ba3e4
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-17 23:03:08 +0200

    style layout

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react';
+import styles from './styles.module.scss';
 import Header from '../Header';
+import Nav from '../Nav';
+import useMediaQuery from '../../hooks/useMediaQuery';
+import defaultAvatar from '../../assets/images/default_avatar.png';
 
 interface Props {
   children?: ReactNode;
@@ -7,12 +11,31 @@ interface Props {
 }
 
 function Layout({ children, title }: Props) {
+  const breakpoint = 700;
+  const matches = useMediaQuery(breakpoint);
+
   return (
     <>
       <Header title={title} />
-      <aside>Left Aside</aside>
-      <main>{children}</main>
-      <aside>Right Aside</aside>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          {matches && (
+            <aside className={styles.nav}>
+              <Nav />
+            </aside>
+          )}
+          <main className={styles.main}>{children}</main>
+          {matches && (
+            <aside className={styles.profile}>
+              <img
+                src={defaultAvatar}
+                alt='default user avatar'
+                className={styles.avatar}
+              />
+            </aside>
+          )}
+        </div>
+      </div>
     </>
   );
 }
