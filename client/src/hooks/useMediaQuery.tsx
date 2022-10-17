33be73cd5df956938819ca625c12f70b942ddd3d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-17 23:00:15 +0200

    patch custom hook

--- client/src/hooks/useMediaQuery.tsx
+++ client/src/hooks/useMediaQuery.tsx
@@ -2,7 +2,7 @@ import useViewport from './useViewport';
 
 function useMediaQuery(breakpoint: number, type: 'min' | 'max' = 'min') {
   const { width } = useViewport();
-  return type === 'min' ? width > breakpoint : width < breakpoint;
+  return type === 'min' ? width >= breakpoint : width < breakpoint;
 }
 
 export default useMediaQuery;
