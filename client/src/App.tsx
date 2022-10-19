b255ab0712e25a9d58f771dea9c5f9a4b3346613
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-19 23:51:30 +0200

    init react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import './App.scss';
 import { Router, Route, Outlet, ReactLocation } from '@tanstack/react-location';
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Landing from './components/Landing';
 import Home from './components/Home';
 import Layout from './components/Layout';
+import Search from './components/Search';
+import Explore from './components/Explore';
 
 const location = new ReactLocation();
 const routes: Route[] = [
@@ -19,15 +24,37 @@ const routes: Route[] = [
     ),
   },
   {
+    path: 'search',
+    element: (
+      <Layout title='Search'>
+        <Search />
+      </Layout>
+    ),
+  },
+  {
+    path: 'explore',
+    element: (
+      <Layout title='Explore'>
+        <Explore />
+      </Layout>
+    ),
+  },
+  {
     element: <h1>404 Not Found 💀</h1>,
   },
 ];
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router location={location} routes={routes}>
-      <Outlet />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router location={location} routes={routes}>
+        <Outlet />
+        <ReactLocationDevtools initialIsOpen={false} />
+        <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
