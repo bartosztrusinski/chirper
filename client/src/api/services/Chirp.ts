9254aea472b2779f70c0df567a2c95cf6a80d9cf
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-21 23:44:11 +0200

    add params to fetch

--- client/src/api/services/Chirp.ts
+++ client/src/api/services/Chirp.ts
@@ -1,7 +1,13 @@
 import client from '../client';
 
 const getMany = async () => {
-  const response = await client.get('/chirps');
+  const response = await client.get('/chirps', {
+    params: {
+      expandAuthor: true,
+      userFields: 'username, profile',
+      chirpFields: 'content, createdAt, metrics, replies',
+    },
+  });
   return response.data;
 };
 
