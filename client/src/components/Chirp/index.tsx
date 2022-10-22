2cfe5522d6dfa4eb8f6145aed4b216220cf9c434
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-22 18:30:39 +0200

    style chirp metrics

--- client/src/components/Chirp/index.tsx
+++ client/src/components/Chirp/index.tsx
@@ -1,8 +1,11 @@
 import styles from './styles.module.scss';
 import defaultAvatar from '../../assets/images/default_avatar.png';
 import { Link, useNavigate } from '@tanstack/react-location';
-import formatRelativeTime from './utils';
+import utils from './utils';
 import { MouseEvent } from 'react';
+import { FaRegCommentAlt } from '@react-icons/all-files/fa/FaRegCommentAlt';
+import { FaRegHeart } from '@react-icons/all-files/fa/FaRegHeart';
+import { FiShare } from '@react-icons/all-files/fi/FiShare';
 
 interface User {
   username: string;
@@ -72,15 +75,22 @@ function Chirp({ chirp }: Props) {
           </span>
           <span className={styles.dot}>·</span>
           <time className={styles.time} dateTime={createdAt}>
-            {formatRelativeTime(new Date(createdAt))}
+            {utils.formatRelativeTime(new Date(createdAt))}
           </time>
         </div>
         <div className={styles.content}>{content}</div>
-        <div>
-          <span>
-            Likes:{metrics.likeCount}
-            Replies: {replies.length}
-          </span>
+        <div className={styles.metrics}>
+          <div className={styles.replies}>
+            <FaRegCommentAlt />
+            <div>{utils.formatCount(replies.length || 124000)}</div>
+          </div>
+          <div className={styles.likes}>
+            <FaRegHeart />
+            <div>{utils.formatCount(metrics.likeCount || 1460)}</div>
+          </div>
+          <div className={styles.share}>
+            <FiShare />
+          </div>
         </div>
       </div>
     </article>
