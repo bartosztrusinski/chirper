e7dfbae9f630856785837f4fdee65f6835f372f8
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-23 23:34:35 +0200

    add singular chirp fetch service

--- client/src/api/services/Chirp.ts
+++ client/src/api/services/Chirp.ts
@@ -1,16 +1,29 @@
 import client from '../client';
 
-const getMany = async () => {
-  const response = await client.get('/chirps', {
-    params: {
-      expandAuthor: true,
-      userFields: 'username, profile',
-      chirpFields: 'content, createdAt, metrics, replies',
-    },
-  });
+const getMany = async (ids?: string[]) => {
+  const params: Record<string, unknown> = {
+    expandAuthor: true,
+    userFields: 'username, profile',
+    chirpFields: 'content, createdAt, metrics, replies',
+  };
+  if (ids) params.ids = ids;
+
+  const response = await client.get('/chirps', { params });
+  return response.data;
+};
+
+const getOne = async (id: string) => {
+  const params = {
+    expandAuthor: true,
+    userFields: 'username, profile',
+    chirpFields: 'content, createdAt, metrics, replies',
+  };
+
+  const response = await client.get(`/chirps/${id}`, { params });
   return response.data;
 };
 
 export default {
   getMany,
+  getOne,
 };
