ebb9e56bb4e84ca81bbdc4116d73707c3e200eea
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-10-30 20:15:13 +0100

    update dark mode hook to change html attr

--- client/src/hooks/useDarkMode.tsx
+++ client/src/hooks/useDarkMode.tsx
@@ -1,13 +1,21 @@
+import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 import useMediaQuery from './useMediaQuery';
 
-const useDarkMode = (defaultValue = false) => {
+const useDarkMode = (initialValue = false) => {
   const prefersDarkMode = usePrefersDarkMode();
 
   const [isDarkMode, setIsDarkMode] = useLocalStorage<boolean>(
     'dark-mode',
-    defaultValue || prefersDarkMode,
+    initialValue || prefersDarkMode,
   );
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      'data-theme',
+      isDarkMode ? 'dark' : 'light',
+    );
+  }, [isDarkMode]);
 
   return {
     toggleDarkMode: () => setIsDarkMode((prev) => !prev),
