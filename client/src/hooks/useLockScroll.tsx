672034e7e4f4aad3aace6e1bf127f69feb85e8ca
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-02 21:48:35 +0100

    add hook for scroll lock with body-scroll-lock

---
+++ client/src/hooks/useLockScroll.tsx
@@ -0,0 +1,23 @@
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+
+const useLockBodyScroll = () => {
+  const lockScroll = (element: HTMLElement) => {
+    disableBodyScroll(element, { reserveScrollBarGap: true });
+  };
+
+  const unlockScroll = (element: HTMLElement) => {
+    enableBodyScroll(element);
+  };
+
+  const clearLocks = () => {
+    clearAllBodyScrollLocks();
+  };
+
+  return [lockScroll, unlockScroll, clearLocks] as const;
+};
+
+export default useLockBodyScroll;
