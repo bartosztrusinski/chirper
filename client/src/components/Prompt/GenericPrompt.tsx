8873a51621cf5bd338a5c2b8b62aaeebd9599935
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-07 21:07:01 +0100

    create generic prompt

---
+++ client/src/components/Prompt/GenericPrompt.tsx
@@ -0,0 +1,12 @@
+import Prompt from '.';
+
+const GenericPrompt = () => {
+  return (
+    <Prompt
+      title="Don't miss what's happening"
+      description='People on Chirper are the first to know.'
+    />
+  );
+};
+
+export default GenericPrompt;
