dbb488a46d2c4234e2bb3ddc6ebdda22f5c869ac
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-07 21:12:56 +0100

    add title panel to modal

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useRef, ReactNode } from 'react';
 import styles from './styles.module.scss';
 import { RiCloseFill as CloseIcon } from '@react-icons/all-files/ri/RiCloseFill';
+import { RiTwitterLine as ChirperIcon } from '@react-icons/all-files/ri/RiTwitterLine';
 import useLockScroll from '../../hooks/useLockScroll';
 
 interface Props {
   open: boolean;
   onClose: () => void;
   children: ReactNode;
+  title?: string;
 }
 
-const Modal = ({ open, onClose, children }: Props) => {
+const Modal = ({ open, onClose, children, title }: Props) => {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   const [lockScroll, unlockScroll, clearLocks] = useLockScroll();
@@ -41,10 +43,17 @@ const Modal = ({ open, onClose, children }: Props) => {
       onClose={onClose}
       className={styles.dialog}
     >
-      <button type='button' onClick={onClose} className={styles.closeButton}>
-        <CloseIcon />
-      </button>
-      <div>{children}</div>
+      <div className={styles.content}>{children}</div>
+      <div className={styles.panel}>
+        {title ? (
+          <div className={styles.title}>{title}</div>
+        ) : (
+          <ChirperIcon className={styles.icon} />
+        )}
+        <button type='button' onClick={onClose} className={styles.closeButton}>
+          <CloseIcon />
+        </button>
+      </div>
     </dialog>
   );
 };
