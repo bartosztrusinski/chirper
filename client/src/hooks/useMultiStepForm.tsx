df4e691800c8d180186915981fbd556b16f94515
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-08 20:33:34 +0100

    create hook for multistep form

---
+++ client/src/hooks/useMultiStepForm.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, useState } from 'react';
+
+const useMultiStepForm = (steps: ReactNode[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const firstStepIndex = 0;
+  const lastStepIndex = steps.length - 1;
+
+  const next = () => {
+    if (currentStepIndex < lastStepIndex) {
+      setCurrentStepIndex((prevStep) => prevStep + 1);
+    }
+  };
+
+  const back = () => {
+    if (currentStepIndex > firstStepIndex) {
+      setCurrentStepIndex((prevStep) => prevStep - 1);
+    }
+  };
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    next,
+    back,
+    steps,
+    isFirstStep: currentStepIndex === firstStepIndex,
+    isLastStep: currentStepIndex === lastStepIndex,
+  };
+};
+
+export default useMultiStepForm;
