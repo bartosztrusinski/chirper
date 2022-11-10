3c94c5ded5891df8ce1c14456e2c40180675c587
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-10 17:57:14 +0100

    update login to use new components

--- client/src/components/LoginForm/index.tsx
+++ client/src/components/LoginForm/index.tsx
@@ -1,40 +1,16 @@
-import Button from '../Button';
 import styles from './styles.module.scss';
-import { BiShow as ShowIcon } from '@react-icons/all-files/bi/BiShow';
-import { BiHide as HideIcon } from '@react-icons/all-files/bi/BiHide';
-import { useState } from 'react';
+import Input from '../Input';
+import PasswordInput from '../PasswordInput';
+import Button from '../Button';
 import { Link } from '@tanstack/react-location';
 
 const LoginForm = () => {
-  const [isPasswordRevealed, setIsPasswordRevealed] = useState(false);
-
-  const togglePasswordReveal = () => {
-    setIsPasswordRevealed((prev) => !prev);
-  };
-
   return (
     <>
-      <div className={styles.heading}>Sign in to Chirper</div>
       <form className={styles.form}>
-        <label className={styles.inputContainer}>
-          <input type='text' className={styles.input} required autoFocus />
-          <div className={styles.placeholder}>Username or email</div>
-        </label>
-        <label className={styles.inputContainer}>
-          <input
-            type={isPasswordRevealed ? 'text' : 'password'}
-            className={styles.input}
-            required
-          />
-          <div className={styles.placeholder}>Password</div>
-          <button
-            type='button'
-            className={styles.revealPassword}
-            onClick={togglePasswordReveal}
-          >
-            {isPasswordRevealed ? <HideIcon /> : <ShowIcon />}
-          </button>
-        </label>
+        <div className={styles.heading}>Sign in to Chirper</div>
+        <Input placeholder='Username or email' />
+        <PasswordInput />
         <Button type='submit' className={styles.submit}>
           Log In
         </Button>
