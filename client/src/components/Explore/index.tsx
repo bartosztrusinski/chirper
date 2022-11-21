cd1e9c5a71bc4fce89337024f03fed62deeac569
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-21 18:36:54 +0100

    fetch chirps for explore page

--- client/src/components/Explore/index.tsx
+++ client/src/components/Explore/index.tsx
@@ -1,11 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import ChirpList from '../ChirpList';
+import ChirpService from '../../api/services/Chirp';
 
-function Explore() {
+const Explore = () => {
+  const {
+    data: chirps,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(['chirps', 'explore'], () => ChirpService.getMany());
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        {error instanceof Error
+          ? error.message
+          : 'Oops something went wrong...'}
+      </div>
+    );
+  }
+
   return (
     <>
-      <ChirpList />
+      <ChirpList chirps={chirps.data} />
     </>
   );
-}
+};
 
 export default Explore;
