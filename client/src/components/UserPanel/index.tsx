3a999190e920ac7390264e76b15c870224311902
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-21 18:51:51 +0100

    add panel for logged in user

--- client/src/components/UserPanel/index.tsx
+++ client/src/components/UserPanel/index.tsx
@@ -1,28 +1,82 @@
-import useMediaQuery from '../../hooks/useMediaQuery';
+import { Link, useNavigate } from '@tanstack/react-location';
+import useUser from '../../hooks/useUser';
 import Button from '../Button';
 import styles from './styles.module.scss';
 import defaultAvatar from '../../assets/images/default_avatar.png';
+import useAuth from '../../hooks/useAuth';
 
-function UserPanel() {
-  const mediumBreakpoint = 700;
-  const largeBreakpoint = 900;
-  const isMediumUp = useMediaQuery(`(min-width: ${mediumBreakpoint}px)`);
-  const isLargeUp = useMediaQuery(`(min-width: ${largeBreakpoint}px)`);
+const UserPanel = () => {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { logOut } = useAuth();
 
-  const userContent = isLargeUp ? (
-    <div className={styles.group}>
-      <Button variant='light'>Sign up</Button>
-      <Button variant='light'>Log in</Button>
+  return user ? (
+    <div className={styles.userPanel}>
+      <Link to={`/users/${user.username}`} className={styles.userContainer}>
+        <img
+          src={user.profile.picture ?? defaultAvatar}
+          alt={`${user.username}'s avatar`}
+          className={styles.avatar}
+        />
+        <div className={styles.usernames}>
+          <div className={styles.name}>{user.profile.name}</div>
+          <div className={styles.username}>@{user.username}</div>
+        </div>
+      </Link>
+
+      <div className={styles.followContainer}>
+        <div>
+          <div className={styles.count}>
+            {user.metrics.followedCount || '14.2K'}
+          </div>
+          Followed
+        </div>
+        <div className={styles.line}></div>
+        <div>
+          <div className={styles.count}> 234{user.metrics.followingCount} </div>
+          Following
+        </div>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <Button
+          variant='light'
+          onClick={() =>
+            navigate({ to: `/users/${user.username}/edit-profile` })
+          }
+        >
+          Edit Profile
+        </Button>
+        <Button
+          variant='light'
+          type='button'
+          onClick={(e) => {
+            e.preventDefault();
+            logOut();
+          }}
+        >
+          Log Out
+        </Button>
+      </div>
     </div>
-  ) : isMediumUp ? (
-    <img
-      src={defaultAvatar}
-      alt='default user avatar'
-      className={styles.avatar}
-    />
-  ) : null;
-
-  return userContent;
-}
+  ) : (
+    <div className={styles.buttonContainer}>
+      <Button
+        variant='light'
+        onClick={() =>
+          navigate({ search: (old) => ({ ...old, signup: true }) })
+        }
+      >
+        Sign up
+      </Button>
+      <Button
+        variant='light'
+        onClick={() => navigate({ search: (old) => ({ ...old, login: true }) })}
+      >
+        Log in
+      </Button>
+    </div>
+  );
+};
 
 export default UserPanel;
