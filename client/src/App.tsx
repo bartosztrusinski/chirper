5070718beb42b734efa5408aaa39f5265471505d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-23 18:25:42 +0100

    move router to app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,99 @@
 import './App.scss';
-import LoginForm from './components/LoginForm';
-import Modal from './components/Modal';
-import RegisterForm from './components/RegisterForm';
-import { useEffect, useState } from 'react';
-import {
-  Outlet,
-  useSearch,
-  useNavigate,
-  MakeGenerics,
-} from '@tanstack/react-location';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Search from './components/Search';
+import Explore from './components/Explore';
+import ChirpPage from './components/ChirpPage';
+import Landing from './components/Landing';
+import UserProfile from './components/UserProfilePage';
+import UserChirps from './components/UserChirps';
+import UserLikedChirps from './components/UserChirps/liked';
+import useUser from './hooks/useUser';
+import AuthenticatedApp from './components/AuthenticatedApp';
+import UnauthenticatedApp from './components/UnauthenticatedApp';
+import { ReactLocation, Router, Route, Outlet } from '@tanstack/react-location';
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
 
-type LocationGenerics = MakeGenerics<{
-  Search: {
-    login?: boolean;
-    signup?: boolean;
-  };
-}>;
+const location = new ReactLocation();
+const routes: Route[] = [
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: 'home',
+    element: (
+      <Layout title='Home'>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: 'search',
+    element: (
+      <Layout title='Search'>
+        <Search />
+      </Layout>
+    ),
+  },
+  {
+    path: 'explore',
+    element: (
+      <Layout title='Explore'>
+        <Explore />
+      </Layout>
+    ),
+  },
+  {
+    path: 'chirps/:id',
+    element: (
+      <Layout title='Chirp'>
+        <ChirpPage />
+      </Layout>
+    ),
+  },
+  {
+    path: 'users/:username',
+    element: (
+      <Layout title='User'>
+        <UserProfile />
+      </Layout>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <UserChirps />,
+      },
+      {
+        path: 'with-replies',
+        element: <UserChirps withReplies />,
+      },
+      {
+        path: 'likes',
+        element: <UserLikedChirps />,
+      },
+    ],
+  },
+  {
+    element: (
+      <Layout title='Not Found'>
+        <h1>404 Not Found 💀</h1>
+      </Layout>
+    ),
+  },
+];
 
 const App = () => {
-  const search = useSearch<LocationGenerics>();
-  const navigate = useNavigate<LocationGenerics>();
-  const [isLogInOpen, setIsLogInOpen] = useState<boolean>(false);
-  const [isSignUpOpen, setIsSignUpOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (search.login && search.signup) return;
-
-    setIsLogInOpen(search.login === true);
-    setIsSignUpOpen(search.signup === true);
-  }, [search.login, search.signup]);
+  // const { user } = useUser();
 
   return (
-    <>
+    <Router location={location} routes={routes}>
       <Outlet />
-      <Modal
-        isOpen={isLogInOpen}
-        onClose={() =>
-          navigate({
-            search: (old) => ({ ...old, login: undefined, signup: undefined }),
-          })
-        }
-      >
-        <LoginForm />
-      </Modal>
-      <RegisterForm
-        isOpen={isSignUpOpen}
-        onClose={() =>
-          navigate({
-            search: (old) => ({ ...old, login: undefined, signup: undefined }),
-          })
-        }
+      <ReactLocationDevtools
+        initialIsOpen={false}
+        toggleButtonProps={{ style: { bottom: '60px', opacity: '0.7' } }}
       />
-    </>
+    </Router>
   );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,101 +1,19 @@
 import App from './App';
+import ReactModal from 'react-modal';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Home from './components/Home';
-import Layout from './components/Layout';
-import Search from './components/Search';
-import Explore from './components/Explore';
-import ChirpPage from './components/ChirpPage';
-import Landing from './components/Landing';
-import UserProfile from './components/UserProfilePage';
-import { Router, Route, ReactLocation } from '@tanstack/react-location';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import UserChirps from './components/UserChirps';
-import UserLikedChirps from './components/UserChirps/liked';
-import ReactModal from 'react-modal';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 const queryClient = new QueryClient();
-const location = new ReactLocation();
 ReactModal.setAppElement(rootElement);
-
-const routes: Route[] = [
-  {
-    path: '/',
-    element: <Landing />,
-  },
-  {
-    path: 'home',
-    element: (
-      <Layout title='Home'>
-        <Home />
-      </Layout>
-    ),
-  },
-  {
-    path: 'search',
-    element: (
-      <Layout title='Search'>
-        <Search />
-      </Layout>
-    ),
-  },
-  {
-    path: 'explore',
-    element: (
-      <Layout title='Explore'>
-        <Explore />
-      </Layout>
-    ),
-  },
-  {
-    path: 'chirps/:id',
-    element: (
-      <Layout title='Chirp'>
-        <ChirpPage />
-      </Layout>
-    ),
-  },
-  {
-    path: 'users/:username',
-    element: (
-      <Layout title='User'>
-        <UserProfile />
-      </Layout>
-    ),
-    children: [
-      {
-        path: '/',
-        element: <UserChirps />,
-      },
-      {
-        path: 'with-replies',
-        element: <UserChirps withReplies />,
-      },
-      {
-        path: 'likes',
-        element: <UserLikedChirps />,
-      },
-    ],
-  },
-  {
-    element: <h1>404 Not Found 💀</h1>,
-  },
-];
 
 root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router location={location} routes={routes}>
-        <App />
-        <ReactLocationDevtools
-          initialIsOpen={false}
-          toggleButtonProps={{ style: { bottom: '60px', opacity: '0.7' } }}
-        />
-      </Router>
+      <App />
       <ReactQueryDevtools
         initialIsOpen={false}
         position='bottom-right'
