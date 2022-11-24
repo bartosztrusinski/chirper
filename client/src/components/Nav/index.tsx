17554356f33e2cb0d29a40e4fcfe4a484431ecec
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-24 17:26:01 +0100

    adjust nav to be container for items

--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,39 +1,19 @@
-import { FaHome } from '@react-icons/all-files/fa/FaHome';
-import { FaSearch } from '@react-icons/all-files/fa/FaSearch';
-import { FaHashtag } from '@react-icons/all-files/fa/FaHashtag';
-import NavLink from './NavLink';
 import styles from './styles.module.scss';
+import { ReactElement } from 'react';
 
 interface Props {
   showNames?: boolean;
+  children: ReactElement | ReactElement[];
 }
 
-const Nav = ({ showNames = true }: Props) => {
+const Nav = ({ children, showNames = true }: Props) => {
   return (
     <nav>
       <ul
         role='list'
-        className={`${styles.list} ${showNames && styles.showNames}`}
+        className={`${styles.list} ${showNames ? styles.nameShown : ''}`}
       >
-        <li>
-          <NavLink to='/home' name='Home' Icon={FaHome} showNames={showNames} />
-        </li>
-        <li>
-          <NavLink
-            to='/explore'
-            name='Explore'
-            Icon={FaHashtag}
-            showNames={showNames}
-          />
-        </li>
-        <li>
-          <NavLink
-            to='/search'
-            name='Search'
-            Icon={FaSearch}
-            showNames={showNames}
-          />
-        </li>
+        {children}
       </ul>
     </nav>
   );
