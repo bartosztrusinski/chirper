ad1b0e6b969b83699eca77475dd501705fb08080
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-26 17:50:29 +0100

    add follow logic to user component

--- client/src/components/User/index.tsx
+++ client/src/components/User/index.tsx
@@ -1,57 +1,58 @@
 import Button from '../Button';
-import User from '../../interfaces/User';
+import { User } from '../../interfaces/User';
 import defaultAvatar from '../../assets/images/default_avatar.png';
 import styles from './styles.module.scss';
 import { Link, useNavigate } from '@tanstack/react-location';
+import useUser from '../../hooks/useUser';
 
 interface Props {
   user: User;
+  isFollowed: boolean;
+  onFollow: (username: string) => void;
 }
 
-const User = ({ user }: Props) => {
-  const {
-    profile: { bio, name, picture },
-    username,
-  } = user;
-  const avatar = picture || defaultAvatar;
-
+const User = ({ user, isFollowed, onFollow }: Props) => {
+  const { user: currentUser } = useUser();
   const navigate = useNavigate();
+  const isCurrentUser = currentUser?.username === user.username;
 
   return (
     <div
       className={styles.userContainer}
-      onClick={() => navigate({ to: `/users/${username}` })}
+      onClick={() => navigate({ to: `/users/${user.username}` })}
     >
-      <Link to={`/users/${username}`} className={styles.avatarContainer}>
+      <Link to={`/users/${user.username}`} className={styles.avatarContainer}>
         <img
-          src={avatar}
-          alt={`${username}'s avatar`}
+          src={user.profile.picture ?? defaultAvatar}
+          alt={`${user.username}'s avatar`}
           className={styles.avatar}
         />
       </Link>
       <div className={styles.mainContainer}>
         <div className={styles.topPanel}>
           <div className={styles.usernames}>
-            <Link className={styles.name} to={`/users/${username}`}>
-              {name}
+            <Link className={styles.name} to={`/users/${user.username}`}>
+              {user.profile.name}
             </Link>
-            <Link className={styles.username} to={`/users/${username}`}>
-              @{username}
+            <Link className={styles.username} to={`/users/${user.username}`}>
+              @{user.username}
             </Link>
           </div>
-          <Button variant='light' className={styles.followButton}>
-            Follow
-          </Button>
+          {!isCurrentUser && (
+            <Button
+              type='button'
+              variant='light'
+              className={styles.followButton}
+              onClick={(e) => {
+                e.stopPropagation();
+                onFollow(user.username);
+              }}
+            >
+              {isFollowed ? 'Unfollow' : 'Follow'}
+            </Button>
+          )}
         </div>
-        {bio || (
-          <div>
-            {bio ||
-              `
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos animi
-        vitae molestiae placeat reprehenderit unde vero aliquam eveniet e
-        `}
-          </div>
-        )}
+        {user.profile.bio && <p>{user.profile.bio}</p>}
       </div>
     </div>
   );
