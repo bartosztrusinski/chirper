443372db9d4ebd2066700b822ab51b5a122c0808
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-28 19:16:22 +0100

    use infinite query & scroll

--- client/src/components/UserTimeline/index.tsx
+++ client/src/components/UserTimeline/index.tsx
@@ -1,38 +1,77 @@
 import AuthenticatedChirpList from '../AuthenticatedChirpList';
 import ChirpService from '../../api/services/Chirp';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import useUser from '../../hooks/useUser';
 import { StoredUser } from '../../interfaces/User';
+import { useCallback, useRef } from 'react';
 
 const UserTimeline = () => {
   const { user } = useUser() as { user: StoredUser };
   const queryKeys = [user.username, 'timeline'];
 
   const {
-    data: chirps,
+    data,
     isLoading,
     isError,
-    error,
-  } = useQuery(['chirps', ...queryKeys], () =>
-    ChirpService.getUserTimeline(user.username),
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ['chirps', ...queryKeys],
+    ({ pageParam }) => ChirpService.getUserTimeline(user.username, pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.meta?.nextPage,
+    },
+  );
+
+  const intersectionObserver = useRef<IntersectionObserver>();
+
+  const lastChirpRef = useCallback(
+    (chirp: HTMLElement | null) => {
+      if (isFetchingNextPage) return;
+
+      if (intersectionObserver.current) {
+        intersectionObserver.current.disconnect();
+      }
+
+      intersectionObserver.current = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (chirp) {
+        intersectionObserver.current.observe(chirp);
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage],
   );
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <div>Loading...</div>;
   }
 
   if (isError) {
-    return (
-      <p>
-        Error:
-        {error instanceof Error
-          ? error.message
-          : 'Oops, something went wrong...'}
-      </p>
-    );
+    return <div>Oops something went wrong...</div>;
   }
 
-  return <AuthenticatedChirpList chirps={chirps} queryKeys={queryKeys} />;
+  return (
+    <>
+      {data.pages.map((page, index) => {
+        const isLastPage = index === data.pages.length - 1;
+
+        return (
+          <AuthenticatedChirpList
+            ref={isLastPage ? lastChirpRef : null}
+            key={index}
+            chirps={page.data}
+            queryKeys={queryKeys}
+            page={index}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default UserTimeline;
