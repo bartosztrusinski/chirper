460f39eb9460a6056bec0e6ee025fbe0d2c80082
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-11-28 18:12:38 +0100

    forward last chirp ref & pass page number

--- client/src/components/AuthenticatedChirpList/index.tsx
+++ client/src/components/AuthenticatedChirpList/index.tsx
@@ -1,29 +1,35 @@
 import Chirp from '../Chirp';
 import IChirp from '../../interfaces/Chirp';
+import ComposeChirpModal from '../ComposeChirpModal';
 import useUser from '../../hooks/useUser';
 import useLikeChirp from '../../hooks/useLikeChirp';
 import useLikedChirpIds from '../../hooks/useLikedChirpIds';
+import { forwardRef, useState } from 'react';
 import { StoredUser } from '../../interfaces/User';
-import ComposeChirpModal from '../ComposeChirpModal';
-import { useState } from 'react';
 
-interface ChirpListProps {
+interface AuthenticatedChirpListProps {
   chirps: IChirp[];
   queryKeys: string[];
+  page: number;
 }
 
-const AuthenticatedChirpList = ({ chirps, queryKeys }: ChirpListProps) => {
+type LastChirpRef = HTMLElement | null;
+
+const AuthenticatedChirpList = forwardRef<
+  LastChirpRef,
+  AuthenticatedChirpListProps
+>(function AuthenticatedChirpList({ chirps, queryKeys, page }, ref) {
   const { user } = useUser() as { user: StoredUser };
-  const { likeChirp, unlikeChirp } = useLikeChirp(queryKeys);
-  const [isReplyModalOpen, setIsReplyModalOpen] = useState(false);
-  const [chirpToReplyTo, setChirpToReplyTo] = useState<IChirp | null>(null);
+  const { likeChirp, unlikeChirp } = useLikeChirp(queryKeys, page);
+  const [isReplyModalOpen, setIsReplyModalOpen] = useState<boolean>(false);
+  const [chirpToReplyTo, setChirpToReplyTo] = useState<IChirp>();
 
   const {
     data: likedChirpIds,
     isError,
     isLoading,
   } = useLikedChirpIds(
-    queryKeys,
+    [...queryKeys, `${page}`],
     user.username,
     chirps.map((chirp) => chirp._id),
   );
@@ -33,7 +39,7 @@ const AuthenticatedChirpList = ({ chirps, queryKeys }: ChirpListProps) => {
   }
 
   if (isError) {
-    return <div>Oops something went wrong</div>;
+    return <div>Oops something went wrong...</div>;
   }
 
   const isChirpLiked = (chirpId: string) => likedChirpIds.includes(chirpId);
@@ -53,9 +59,12 @@ const AuthenticatedChirpList = ({ chirps, queryKeys }: ChirpListProps) => {
 
   return (
     <>
-      {chirps.map((chirp) => {
+      {chirps.map((chirp, index) => {
+        const isLastChirp = index === chirps.length - 1;
+
         return (
           <Chirp
+            ref={isLastChirp ? ref : null}
             key={chirp._id}
             chirp={chirp}
             isLiked={isChirpLiked(chirp._id)}
@@ -64,13 +73,14 @@ const AuthenticatedChirpList = ({ chirps, queryKeys }: ChirpListProps) => {
           />
         );
       })}
+
       <ComposeChirpModal
-        chirp={chirpToReplyTo!}
-        open={isReplyModalOpen}
-        onClose={() => setIsReplyModalOpen(false)}
+        replyToChirp={chirpToReplyTo as IChirp}
+        isOpen={isReplyModalOpen}
+        onRequestClose={() => setIsReplyModalOpen(false)}
       />
     </>
   );
-};
+});
 
 export default AuthenticatedChirpList;
