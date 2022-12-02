fb2e0807d7f113fa4fd7704243f2878ee0114010
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-03 00:35:43 +0100

    adjust to form & autosize packages

--- client/src/components/Textarea/index.tsx
+++ client/src/components/Textarea/index.tsx
@@ -1,28 +1,38 @@
-import { ComponentPropsWithoutRef, useRef } from 'react';
-import useAutosizeTextArea from '../../hooks/useAutosizeTextarea';
 import styles from './styles.module.scss';
+import { forwardRef } from 'react';
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from 'react-textarea-autosize';
 
-interface Props extends ComponentPropsWithoutRef<'textarea'> {
+interface TextareaProps extends TextareaAutosizeProps {
   placeholder: string;
+  placeholderClassName?: string;
 }
 
-const Textarea = ({ placeholder, ...restProps }: Props) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  function Textarea(
+    { placeholder, placeholderClassName, className, ...restProps },
+    ref,
+  ) {
+    const labelClasses = [styles.textareaContainer, className]
+      .filter(Boolean)
+      .join(' ');
+    const placeholderClasses = [styles.placeholder, placeholderClassName]
+      .filter(Boolean)
+      .join(' ');
 
-  useAutosizeTextArea(textareaRef.current, restProps.value as string);
-
-  return (
-    <label className={styles.textareaContainer}>
-      <textarea
-        {...restProps}
-        ref={textareaRef}
-        placeholder=''
-        className={styles.textarea}
-        rows={1}
-      />
-      <div className={styles.placeholder}>{placeholder}</div>
-    </label>
-  );
-};
+    return (
+      <label className={labelClasses}>
+        <TextareaAutosize
+          {...restProps}
+          ref={ref}
+          placeholder=''
+          className={styles.textarea}
+        />
+        <div className={placeholderClasses}>{placeholder}</div>
+      </label>
+    );
+  },
+);
 
 export default Textarea;
