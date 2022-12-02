2493ea19e8b0a89b2e74f04bbad604e3e5b6763a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-02 18:30:45 +0100

    add interface & reset function

--- client/src/hooks/useMultiStep.tsx
+++ client/src/hooks/useMultiStep.tsx
@@ -1,32 +1,49 @@
 import { ReactNode, useState } from 'react';
 
-const useMultiStep = (steps: ReactNode[]) => {
+interface UseMultiStep {
+  steps: ReactNode[];
+  currentStepIndex: number;
+  currentStep: ReactNode;
+  isFirstStep: boolean;
+  isLastStep: boolean;
+  nextStep: () => void;
+  previousStep: () => void;
+  resetSteps: () => void;
+}
+
+const useMultiStep = (steps: ReactNode[]): UseMultiStep => {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
   const firstStepIndex = 0;
   const lastStepIndex = steps.length - 1;
 
-  const next = () => {
+  const nextStep = () => {
     if (currentStepIndex < lastStepIndex) {
       setCurrentStepIndex((prevStep) => prevStep + 1);
     }
   };
 
-  const back = () => {
+  const previousStep = () => {
     if (currentStepIndex > firstStepIndex) {
       setCurrentStepIndex((prevStep) => prevStep - 1);
     }
   };
 
+  const resetSteps = () => {
+    setCurrentStepIndex(firstStepIndex);
+  };
+
   return {
-    currentStepIndex,
-    step: steps[currentStepIndex],
-    next,
-    back,
     steps,
+    currentStepIndex,
+    currentStep: steps[currentStepIndex],
     isFirstStep: currentStepIndex === firstStepIndex,
     isLastStep: currentStepIndex === lastStepIndex,
+    nextStep,
+    previousStep,
+    resetSteps,
   };
 };
 
 export default useMultiStep;
+export { UseMultiStep };
