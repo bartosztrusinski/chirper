53d33d92dc8758d990570ef014b32749941c4426
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-05 01:44:00 +0100

    manage form using react-hook-form

--- client/src/components/ComposeChirpForm/index.tsx
+++ client/src/components/ComposeChirpForm/index.tsx
@@ -1,33 +1,44 @@
 import { Link } from '@tanstack/react-location';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import useUser from '../../hooks/useUser';
 import styles from './styles.module.scss';
 import defaultAvatar from '../../assets/images/default_avatar.png';
 import Button from '../Button';
 import { StoredUser } from '../../interfaces/User';
 import TextareaAutosize from 'react-textarea-autosize';
+import { useForm } from 'react-hook-form';
+import { content } from './schemas';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+type Inputs = z.infer<typeof inputsSchema>;
+
+const inputsSchema = z.object({
+  content,
+});
 
 const ComposeChirpForm = () => {
   const { user: currentUser } = useUser() as { user: StoredUser };
-  const [value, setValue] = useState('');
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isDirty },
+  } = useForm<Inputs>({
+    mode: 'onChange',
+    resolver: zodResolver(inputsSchema),
+    defaultValues: { content: '' },
+  });
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const val = e.target.value;
-    setValue(val);
-  };
+  const { ref, ...rest } = register('content');
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log('submit');
+  const onSubmit = (inputs: Inputs) => {
+    console.log(inputs);
   };
 
   return (
-    <div
-      className={styles.container}
-      onClick={() => textAreaRef.current?.focus()}
-    >
-      <div>
+    <div className={styles.container}>
+      <div className={styles.card} onClick={() => contentRef.current?.focus()}>
         <Link to={`/users/${currentUser.username}`}>
           <img
             src={currentUser.profile.picture ?? defaultAvatar}
@@ -35,20 +46,34 @@ const ComposeChirpForm = () => {
             className={styles.avatar}
           />
         </Link>
+
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+          <TextareaAutosize
+            ref={(e) => {
+              ref(e);
+              contentRef.current = e;
+            }}
+            placeholder="What's happening"
+            className={styles.chirpContent}
+            aria-invalid={errors.content ? 'true' : 'false'}
+            {...rest}
+          />
+
+          <Button
+            type='submit'
+            className={styles.submitButton}
+            disabled={!isDirty || !isValid}
+          >
+            Reply
+          </Button>
+        </form>
       </div>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <TextareaAutosize
-          ref={textAreaRef}
-          value={value}
-          onChange={handleChange}
-          placeholder="What's happening"
-          className={styles.textarea}
-          rows={1}
-        />
-        <Button className={styles.submit} disabled={!value} type='submit'>
-          Reply
-        </Button>
-      </form>
+
+      {errors.content && (
+        <p role='alert' className={styles.errorMessage}>
+          {errors.content.message}
+        </p>
+      )}
     </div>
   );
 };
