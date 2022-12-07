7a4ac110fc425305ddeba23119166295584c5773
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-07 04:09:30 +0100

    provide global access to create chirp modal

--- client/src/components/AuthenticatedApp/index.tsx
+++ client/src/components/AuthenticatedApp/index.tsx
@@ -1,7 +1,43 @@
 import { Outlet } from '@tanstack/react-location';
+import { createContext, useState } from 'react';
+import Chirp from '../../interfaces/Chirp';
+import CreateChirpModal from '../CreateChirpModal';
+
+interface CreateChirpContext {
+  replyTo: Chirp | null;
+  openCreateChirpModal: (replyTo?: Chirp) => void;
+  closeCreateChirpModal: () => void;
+}
+
+const CreateChirpContext = createContext<CreateChirpContext | null>(null);
 
 const AuthenticatedApp = () => {
-  return <Outlet />;
+  const [isCreateChirpModalOpen, setIsCreateChirpModalOpen] =
+    useState<boolean>(false);
+  const [replyTo, setReplyTo] = useState<Chirp | null>(null);
+
+  const openCreateChirpModal = (replyTo?: Chirp) => {
+    if (replyTo) setReplyTo(replyTo);
+    setIsCreateChirpModalOpen(true);
+  };
+
+  const closeCreateChirpModal = () => {
+    setReplyTo(null);
+    setIsCreateChirpModalOpen(false);
+  };
+
+  return (
+    <CreateChirpContext.Provider
+      value={{ replyTo, openCreateChirpModal, closeCreateChirpModal }}
+    >
+      <Outlet />
+      <CreateChirpModal
+        isOpen={isCreateChirpModalOpen}
+        onRequestClose={closeCreateChirpModal}
+      />
+    </CreateChirpContext.Provider>
+  );
 };
 
 export default AuthenticatedApp;
+export { CreateChirpContext };
