7f752123a9dcaca76ce824daf7707db43a473db7
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-08 02:15:19 +0100

    add infinite query search based on query params

--- client/src/components/Search/index.tsx
+++ client/src/components/Search/index.tsx
@@ -1,5 +1,103 @@
-function Search() {
-  return <h1>Search Page</h1>;
-}
+import { MakeGenerics, useSearch } from '@tanstack/react-location';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useCallback, useRef } from 'react';
+import ChirpService from '../../api/services/Chirp';
+import useUser from '../../hooks/useUser';
+import AuthenticatedChirpList from '../AuthenticatedChirpList';
+import UnauthenticatedChirpList from '../UnauthenticatedChirpList';
+
+type SearchParams = {
+  query: string;
+  sortOrder?: string;
+  from?: string;
+  includeReplies?: boolean;
+  followedOnly?: boolean;
+  startTime?: string;
+  endTime?: string;
+};
+
+const Search = () => {
+  const search = useSearch<MakeGenerics<{ Search: SearchParams }>>();
+  const { user } = useUser();
+  const queryKeys = ['search', search];
+
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ['chirps', ...queryKeys],
+    ({ pageParam }) =>
+      ChirpService.searchChirps(
+        { ...search, query: search.query ?? '' },
+        pageParam,
+      ),
+    {
+      getNextPageParam: (lastPage) => lastPage.meta?.nextPage,
+    },
+  );
+
+  const intersectionObserver = useRef<IntersectionObserver>();
+
+  const lastChirpRef = useCallback(
+    (chirp: HTMLElement | null) => {
+      if (isFetchingNextPage) return;
+
+      if (intersectionObserver.current) {
+        intersectionObserver.current.disconnect();
+      }
+
+      intersectionObserver.current = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (chirp) {
+        intersectionObserver.current.observe(chirp);
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage],
+  );
+
+  if (!search.query) {
+    return <div>Search for something</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <>
+      {data.pages.map((page, index) => {
+        const isLastPage = index === data.pages.length - 1;
+
+        return user ? (
+          <AuthenticatedChirpList
+            ref={isLastPage ? lastChirpRef : null}
+            key={index}
+            chirps={page.data}
+            queryKeys={queryKeys}
+            page={index}
+          />
+        ) : (
+          <UnauthenticatedChirpList
+            ref={isLastPage ? lastChirpRef : null}
+            key={index}
+            chirps={page.data}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 export default Search;
