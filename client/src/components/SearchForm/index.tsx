8a55ba4e041bab6da7449a5d68f2a9cba6c1e17f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-08 02:16:12 +0100

    make filter button functional

--- client/src/components/SearchForm/index.tsx
+++ client/src/components/SearchForm/index.tsx
@@ -1,8 +1,12 @@
 import styles from './styles.module.scss';
 import { FaSearch } from '@react-icons/all-files/fa/FaSearch';
 import { FaFilter } from '@react-icons/all-files/fa/FaFilter';
+import { useState } from 'react';
+import SearchFilterModal from '../SearchFilterModal';
 
-function SearchForm() {
+const SearchForm = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+
   return (
     <>
       <form className={styles.form}>
@@ -11,15 +15,23 @@ function SearchForm() {
           className={styles.input}
           placeholder='Search Chirper'
         />
-        <button type='button' className={styles.button}>
+        <button
+          type='button'
+          className={styles.button}
+          onClick={() => setIsFilterOpen(true)}
+        >
           <FaFilter />
         </button>
         <button type='submit' className={styles.button}>
           <FaSearch />
         </button>
       </form>
+      <SearchFilterModal
+        isOpen={isFilterOpen}
+        onRequestClose={() => setIsFilterOpen(false)}
+      />
     </>
   );
-}
+};
 
 export default SearchForm;
