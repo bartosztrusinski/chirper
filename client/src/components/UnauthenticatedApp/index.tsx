59d8e06f83f28f181b44d6f92d45c89301c8a7b3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-11 23:49:02 +0100

    add context for reply prompt

--- client/src/components/UnauthenticatedApp/index.tsx
+++ client/src/components/UnauthenticatedApp/index.tsx
@@ -1,13 +1,14 @@
 import LoginForm from '../LoginForm';
 import Modal from '../Modal';
 import RegisterForm from '../RegisterForm';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import {
   Outlet,
   useSearch,
   useNavigate,
   MakeGenerics,
 } from '@tanstack/react-location';
+import ReplyPrompt from '../Prompt/ReplyPrompt';
 
 type LocationGenerics = MakeGenerics<{
   Search: {
@@ -16,11 +17,30 @@ type LocationGenerics = MakeGenerics<{
   };
 }>;
 
+interface PromptContext {
+  openReplyPrompt: (username: string) => void;
+  closeReplyPrompt: () => void;
+}
+
+const PromptContext = createContext<PromptContext | null>(null);
+
 const UnauthenticatedApp = () => {
   const search = useSearch<LocationGenerics>();
   const navigate = useNavigate<LocationGenerics>();
   const [isLogInOpen, setIsLogInOpen] = useState<boolean>(false);
   const [isSignUpOpen, setIsSignUpOpen] = useState<boolean>(false);
+  const [isReplyPromptOpen, setIsReplyPromptOpen] = useState<boolean>(false);
+  const [promptUsername, setPromptUsername] = useState<string>('');
+
+  const openReplyPrompt = (username: string) => {
+    setPromptUsername(username);
+    setIsReplyPromptOpen(true);
+  };
+
+  const closeReplyPrompt = () => {
+    setPromptUsername('');
+    setIsReplyPromptOpen(false);
+  };
 
   useEffect(() => {
     if (search.login && search.signup) return;
@@ -31,7 +51,9 @@ const UnauthenticatedApp = () => {
 
   return (
     <>
-      <Outlet />
+      <PromptContext.Provider value={{ openReplyPrompt, closeReplyPrompt }}>
+        <Outlet />
+      </PromptContext.Provider>
       <Modal
         isOpen={isLogInOpen}
         onRequestClose={() =>
@@ -48,7 +70,7 @@ const UnauthenticatedApp = () => {
       </Modal>
       <RegisterForm
         isOpen={isSignUpOpen}
-        onClose={() =>
+        onRequestClose={() =>
           navigate({
             search: (old) => ({
               ...old,
@@ -58,8 +80,16 @@ const UnauthenticatedApp = () => {
           })
         }
       />
+      <Modal
+        isOpen={isReplyPromptOpen}
+        onRequestClose={() => setIsReplyPromptOpen(false)}
+        title=' '
+      >
+        <ReplyPrompt username={promptUsername} />
+      </Modal>
     </>
   );
 };
 
 export default UnauthenticatedApp;
+export { PromptContext };
