b99bf3bfad1071db4c9deae38af134c6211c5c8d
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-12 02:35:01 +0100

    get user using hook & disable if no user

--- client/src/hooks/useFollowedUsernames.tsx
+++ client/src/hooks/useFollowedUsernames.tsx
@@ -1,17 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import UserService from '../api/services/User';
+import useUser from './useUser';
 
-const useFollowedUsernames = (
-  queryKeys: unknown[],
-  username: string,
-  userIds: string[],
-) =>
-  useQuery(
+const useFollowedUsernames = (queryKeys: unknown[], userIds: string[]) => {
+  const { user } = useUser();
+
+  return useQuery(
     ['followedUsernames', ...queryKeys],
-    () => UserService.getFollowedUsernames(username, userIds),
-    {
-      enabled: !!userIds.length,
-    },
+    () => UserService.getFollowedUsernames(user!.username, userIds),
+    { enabled: Boolean(userIds.length && user) },
   );
+};
 
 export default useFollowedUsernames;
