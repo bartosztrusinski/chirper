f9724c98d889376c2c5dcd6324a83db03828a43b
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-16 23:31:42 +0100

    add log out & new chirp buttons

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -8,13 +8,20 @@ import SearchForm from '../SearchForm';
 import AuthenticatedNav from '../Nav/AuthenticatedNav';
 import UnauthenticatedNav from '../Nav/UnauthenticatedNav';
 import useUser from '../../hooks/useUser';
+import Button from '../Button';
+import { useNavigate } from '@tanstack/react-location';
+import { BiLogOut as LogOutIcon } from '@react-icons/all-files/bi/BiLogOut';
+import { IoMdAdd as CreateChirpIcon } from '@react-icons/all-files/io/IoMdAdd';
+import useAuth from '../../hooks/useAuth';
 
 interface LayountProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayountProps) => {
+  const navigate = useNavigate();
   const { user } = useUser();
+  const { logOut } = useAuth();
 
   const smallBreakpoint = 536;
   const largeBreakpoint = 940;
@@ -28,9 +35,36 @@ const Layout = ({ children }: LayountProps) => {
       <Header />
       <div className={styles.grid}>
         {isSmallUp && (
-          <Sidebar>
+          <Sidebar className={styles.nav}>
             {user ? (
-              <AuthenticatedNav showNames={isXLargeUp} />
+              <>
+                <AuthenticatedNav showNames={isXLargeUp} />
+
+                <Button
+                  className={styles.button}
+                  onClick={() =>
+                    navigate({
+                      search: (old) => ({ ...old, dialog: 'create-chirp' }),
+                    })
+                  }
+                >
+                  {isXLargeUp ? (
+                    'Chirp'
+                  ) : (
+                    <CreateChirpIcon className={styles.icon} />
+                  )}
+                </Button>
+
+                {!isLargeUp && (
+                  <Button
+                    variant='dark'
+                    className={styles.button}
+                    onClick={logOut}
+                  >
+                    <LogOutIcon className={styles.icon} />
+                  </Button>
+                )}
+              </>
             ) : (
               <UnauthenticatedNav showNames={isXLargeUp} />
             )}
