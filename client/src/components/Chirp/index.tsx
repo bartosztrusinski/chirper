3c0db8ff80a8f839883b2d6d5c97e81914e567ea
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-17 04:12:59 +0100

    improve accessibility & semantics

--- client/src/components/Chirp/index.tsx
+++ client/src/components/Chirp/index.tsx
@@ -2,8 +2,8 @@ import styles from './styles.module.scss';
 import defaultAvatar from '../../assets/images/default_avatar.png';
 import utils from '../../utils/utils';
 import Chirp from '../../interfaces/Chirp';
-import { forwardRef, MouseEvent, useContext } from 'react';
-import { Link, useNavigate } from '@tanstack/react-location';
+import { forwardRef, useContext } from 'react';
+import { Link } from '@tanstack/react-location';
 import { FaRegCommentAlt } from '@react-icons/all-files/fa/FaRegCommentAlt';
 import { FaRegHeart } from '@react-icons/all-files/fa/FaRegHeart';
 import { FiShare } from '@react-icons/all-files/fi/FiShare';
@@ -26,100 +26,88 @@ const Chirp = forwardRef<Ref, ChirpProps>(function Chirp(
 ) {
   const createChirpContext = useContext(CreateChirpContext);
   const promptContext = useContext(PromptContext);
-  const navigate = useNavigate();
   const { user } = useUser();
 
-  const { _id, content, createdAt, author, metrics, replies } = chirp;
-  const { username, profile } = author;
-  const avatar = profile.picture ?? defaultAvatar;
-  const relativeCreatedTime = utils.formatRelativeTime(createdAt);
-
-  const stopPropagation = (e: MouseEvent) => e.stopPropagation();
-
   return (
-    <article
-      ref={ref}
-      onClick={() => navigate({ to: `/chirps/${_id}` })}
-      className={styles.chirp}
-    >
-      <Link to={`/users/${username}`} onClick={stopPropagation}>
-        <img
-          src={avatar}
-          alt={`${username}'s avatar`}
-          className={styles.avatar}
-        />
-      </Link>
+    <article ref={ref}>
+      <Link to={`/chirps/${chirp._id}`} className={styles.chirp}>
+        <Link to={`/users/${chirp.author.username}`}>
+          <img
+            src={chirp.author.profile.picture ?? defaultAvatar}
+            alt={`${chirp.author.username}'s avatar`}
+            className={styles.avatar}
+          />
+        </Link>
 
-      <div className={styles.mainContainer}>
-        <div className={styles.upperPanel}>
-          <div>
-            <Link
-              to={`/users/${username}`}
-              className={styles.name}
-              onClick={stopPropagation}
-            >
-              {profile.name}
-            </Link>
-            <Link
-              to={`/users/${username}`}
-              className={styles.username}
-              onClick={stopPropagation}
-            >
-              @{username}
-            </Link>
+        <div className={styles.mainContainer}>
+          <div className={styles.upperPanel}>
+            <div>
+              <Link
+                to={`/users/${chirp.author.username}`}
+                className={styles.name}
+              >
+                {chirp.author.profile.name}
+              </Link>
+              <Link
+                to={`/users/${chirp.author.username}`}
+                className={styles.username}
+              >
+                @{chirp.author.username}
+              </Link>
+            </div>
+            <div>·</div>
+            <time className={styles.time} dateTime={chirp.createdAt}>
+              {utils.formatRelativeTime(chirp.createdAt)}
+            </time>
           </div>
-          <div>·</div>
-          <time className={styles.time} dateTime={createdAt}>
-            {relativeCreatedTime}
-          </time>
+
+          <p className={styles.content}>{chirp.content}</p>
+
+          {showMetrics && (
+            <div className={styles.metrics}>
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  user
+                    ? createChirpContext?.openCreateChirpModal(chirp)
+                    : promptContext?.openReplyPrompt(chirp.author.username);
+                }}
+                className={styles.replies}
+              >
+                <button className={styles.iconBackground}>
+                  <FaRegCommentAlt className={styles.icon} />
+                </button>
+                <div>{utils.formatCount(chirp.replies.length)}</div>
+              </div>
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  onLike();
+                }}
+                className={`${styles.likes} ${isLiked ? styles.liked : ''}`}
+              >
+                <button className={styles.iconBackground}>
+                  <FaRegHeart className={styles.icon} />
+                </button>
+                <div>{utils.formatCount(chirp.metrics.likeCount)}</div>
+              </div>
+              <div
+                className={styles.share}
+                onClick={(e) => {
+                  e.preventDefault();
+                  navigator.clipboard.writeText(
+                    `${window.location.origin}/chirps/${chirp._id}`,
+                  );
+                }}
+              >
+                <button className={styles.iconBackground}>
+                  <FiShare className={styles.icon} />
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-
-        <p className={styles.content}>{content}</p>
-
-        {showMetrics && (
-          <div className={styles.metrics}>
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                user
-                  ? createChirpContext?.openCreateChirpModal(chirp)
-                  : promptContext?.openReplyPrompt(chirp.author.username);
-              }}
-              className={styles.replies}
-            >
-              <div className={styles.iconBackground}>
-                <FaRegCommentAlt className={styles.icon} />
-              </div>
-              <div>{utils.formatCount(replies.length)}</div>
-            </div>
-            <div
-              onClick={(e) => {
-                e.stopPropagation();
-                onLike();
-              }}
-              className={`${styles.likes} ${isLiked ? styles.liked : ''}`}
-            >
-              <div className={styles.iconBackground}>
-                <FaRegHeart className={styles.icon} />
-              </div>
-              <div>{utils.formatCount(metrics.likeCount)}</div>
-            </div>
-            <div
-              className={styles.share}
-              onClick={(e) => {
-                e.stopPropagation();
-                navigator.clipboard.writeText(
-                  `${window.location.origin}/chirps/${_id}`,
-                );
-              }}
-            >
-              <div className={styles.iconBackground}>
-                <FiShare className={styles.icon} />
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
+      </Link>
     </article>
   );
 });
