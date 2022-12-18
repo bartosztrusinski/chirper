cb557df40943f8896b80408b51e4ee2432ecd2a1
Bartosz Trusiński <bartosztrusinski@gmail.com>
2022-12-18 01:14:32 +0100

    create loader component

---
+++ client/src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.spinner}>
+      <div className={styles.doubleBounce1}></div>
+      <div className={styles.doubleBounce2}></div>
+    </div>
+  );
+};
+
+export default Loader;
