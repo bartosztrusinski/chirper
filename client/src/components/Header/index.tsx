8b272cc0c8de0b9c67a52691cce748a06699eb29
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-01-08 00:22:33 +0100

    use breakpoint hook; new menu icon; show search form on mobile search page

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,16 +1,17 @@
 import styles from './styles.module.scss';
 import { RiTwitterLine as ChirperIcon } from '@react-icons/all-files/ri/RiTwitterLine';
 import SearchForm from '../SearchForm';
-import useMediaQuery from '../../hooks/useMediaQuery';
+import useBreakpoint from '../../hooks/useBreakpoint';
 import DarkModeToggle from '../Toggle/DarkModeToggle';
 import { useEffect, useState } from 'react';
-import { FiMenu } from '@react-icons/all-files/fi/FiMenu';
+import { TiThMenuOutline as MenuIcon } from '@react-icons/all-files/ti/TiThMenuOutline';
 import MenuModal from '../MenuModal';
 import useUser from '../../hooks/useUser';
 import defaultAvatar from '../../assets/images/default_avatar.png';
 import {
   Link,
   MakeGenerics,
+  useLocation,
   useNavigate,
   useSearch,
 } from '@tanstack/react-location';
@@ -23,14 +24,11 @@ type LocationGenerics = MakeGenerics<{
 
 const Header = () => {
   const navigate = useNavigate<LocationGenerics>();
+  const location = useLocation<LocationGenerics>();
   const { dialog } = useSearch<LocationGenerics>();
   const { user } = useUser();
-
-  const smallBreakpoint = 536;
-  const mediumBreakpoint = 690;
-  const isSmallUp = useMediaQuery(`(min-width: ${smallBreakpoint}px)`);
-  const isMediumUp = useMediaQuery(`(min-width: ${mediumBreakpoint}px)`);
-
+  const isScreenSmallUp = useBreakpoint('up', 'small');
+  const isScreenMediumUp = useBreakpoint('up', 'medium');
   const [title, setTitle] = useState<string>('');
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(dialog === 'menu');
 
@@ -42,59 +40,66 @@ const Header = () => {
     setIsMenuOpen(dialog === 'menu');
   }, [dialog]);
 
+  const isSearchPage = location.current.pathname === '/search';
+
+  const closeDialog = () => {
+    navigate({
+      search: (old) => ({ ...old, dialog: undefined }),
+      replace: true,
+    });
+  };
+
   return (
     <header className={styles.header}>
       <Link to='/' className={styles.logo}>
         <div
-          className={`${styles.name} ${isMediumUp ? '' : 'visually-hidden'}`}
+          className={`${styles.name} ${
+            isScreenMediumUp ? '' : 'visually-hidden'
+          }`}
         >
           Chirper
         </div>
         <ChirperIcon className={styles.icon} />
       </Link>
 
-      <h1 className={`${styles.heading} ${isSmallUp ? 'visually-hidden' : ''}`}>
+      <h1
+        className={`${styles.heading} ${
+          isSearchPage || isScreenSmallUp ? 'visually-hidden' : ''
+        }`}
+      >
         {title}
       </h1>
 
-      {isSmallUp ? (
-        <>
-          <SearchForm />
-          <DarkModeToggle />
-        </>
-      ) : (
-        <>
-          <button
-            className={styles.menu}
-            onClick={() =>
-              navigate({
-                search: (old) => ({ ...old, dialog: 'menu' }),
-                replace: true,
-              })
-            }
-          >
-            {user ? (
-              <img
-                className={styles.image}
-                src={user.profile.picture ?? defaultAvatar}
-                alt={`${user.username}'s avatar`}
-              />
-            ) : (
-              <FiMenu className={styles.icon} />
-            )}
-          </button>
+      {(isSearchPage || isScreenSmallUp) && <SearchForm />}
 
-          <MenuModal
-            isOpen={isMenuOpen}
-            onRequestClose={() =>
-              navigate({
-                search: (old) => ({ ...old, dialog: undefined }),
-                replace: true,
-              })
-            }
-          />
-        </>
+      {isScreenSmallUp ? (
+        <DarkModeToggle />
+      ) : (
+        <button
+          className={styles.menu}
+          onClick={() =>
+            navigate({
+              search: (old) => ({ ...old, dialog: 'menu' }),
+              replace: true,
+            })
+          }
+        >
+          {user ? (
+            <img
+              className={styles.image}
+              src={user.profile.picture ?? defaultAvatar}
+              alt={`${user.username}'s avatar`}
+            />
+          ) : (
+            <MenuIcon className={styles.icon} />
+          )}
+        </button>
       )}
+
+      <MenuModal
+        isOpen={isMenuOpen && !isScreenSmallUp}
+        onRequestClose={closeDialog}
+      />
     </header>
   );
 };
