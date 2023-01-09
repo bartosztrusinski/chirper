a057d086b1727d551c6ac008429b7c6c6b5e5879
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-01-09 02:36:49 +0100

    create container component

---
+++ client/src/components/Container/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+import { ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return <div className={styles.container}>{children}</div>;
+};
+
+export default Container;
