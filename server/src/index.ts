fbe39642503f9c0c4c87380c2077a1e1d1c6b79a
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-11 02:32:21 +0200

    configure vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
 import 'dotenv/config';
 import connectDB from './config/db.config';
-import envConfig from './config/env.config';
 import app from './app';
 
 connectDB();
 
-const port = envConfig.app.port;
-const env = envConfig.app.environment;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} in ${env} mode`);
-});
+export default app;
